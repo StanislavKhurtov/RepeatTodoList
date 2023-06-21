c9704ef8a7263ba353afc181013ecc3c7d52e8f7
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-06-21 11:13:15 +0300

    feat: add props tasks componenta Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,23 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
+    let tasks1 = [
+        {id: 1, title: "CSS", isDone: true},
+        {id: 2, title: "CSS", isDone: true},
+        {id: 3, title: "CSS", isDone: true},
+
+    ]
+    let tasks2 = [
+        {id: 1, title: "Terminator", isDone: true},
+        {id: 2, title: "John Weak", isDone: false},
+        {id: 3, title: "XXX", isDone: true},
+
+    ]
 
     return (
         <div className="App">
-            <Todolist title={"What to learn"}/>
-            <Todolist title={"Movies"}/>
+            <Todolist title={"What to learn"} tasks={tasks1}/>
+            <Todolist title={"Movies"} tasks={tasks2}/>
         </div>
     );
 }
