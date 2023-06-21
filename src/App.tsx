d50990b4cee7bd0cc16baec89da8ca8f886c7896
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-06-21 10:25:14 +0300

    feat: add componenta

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            <Todolist/>
+        </div>
+    );
+}
+
+const Todolist = () => {
+    return (
+        <div>Todolist</div>
+    );
 }
 
 export default App;
