4a2061f43f442067ddf7013598d35427e6c38361
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-06-21 10:38:14 +0300

    refactor: change structure project

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,36 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    debugger
+
     return (
         <div className="App">
+            <Todolist/>
+            <Todolist/>
             <Todolist/>
         </div>
     );
 }
 
 const Todolist = () => {
-    debugger
+
     return (
-        <div>Todolist</div>
+        <div>
+            <h3>What to learn</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true}/><span>HTML&CSS</span></li>
+                <li><input type="checkbox" checked={true}/><span>JavaScript</span></li>
+                <li><input type="checkbox" checked={false}/><span>React</span></li>
+            </ul>
+            <div >
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
     );
 }
 
