79cb2a07d7daf68a7db67c79e5d2b8dd565156a0
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-15 13:16:10 +0300

    refactor: change structure

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,24 @@ export const App = () => {
 
 const Todolist = () => {
     return (
-        <div>Hello</div>
+        <div>
+            <h3>What to learn</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/><span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/><span>JavaScript</span></li>
+                    <li><input type="checkbox" checked={false}/><span>React</span></li>
+                </ul>
+            </div>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Comleted</button>
+            </div>
+        </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
 
-debugger
+
 ReactDOM.render(<App/>, document.getElementById('root'));
