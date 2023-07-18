4e42800aac5c6f1c7bd0de4cfeec2bd38c09ac20
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-18 11:32:05 +0300

    feat:lesson #7 add appBar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import {TaskType, Todolist} from "./Todolist";
 import {v1} from "uuid";
 import {AddItemForm} from "./AddItemForm";
+import {AppBar, Button, IconButton, Toolbar, Typography} from "@material-ui/core";
+import {Menu} from "@material-ui/icons";
 
 
 export type TodolistType = {
@@ -84,6 +86,19 @@ export const App = () => {
 
     return (
         <div className="App">
+
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                        <Menu />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+
             <AddItemForm addItem={addTodolist}/>
 
             {todolists.map((el) => {
