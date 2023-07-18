a697e63cd350b71bb759a900cb4a021df84dda44
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-18 16:49:26 +0300

    feat:lesson #8 change type action

--- src/state/todolist-reducer.ts
+++ src/state/todolist-reducer.ts
@@ -1,10 +1,33 @@
-import {TodolistType} from "../App";
+import {FilterValueType, TodolistType} from "../App";
 import {v1} from "uuid";
 
-type ActionType = {
+/*type ActionType = {
     type: string
     [key: string]: any
-}
+}*/
+
+export type RemoveTodolistActionType = {
+    type: 'REMOVE-TODOLIST'
+    id: string
+};
+
+export type AddTodolistActionType = {
+    type: 'ADD-TODOLIST'
+    title: string
+};
+
+export type ChangeTodolistTitleActionType = {
+    type: 'CHANGE-TODOLIST-TITLE'
+    id: string
+    title: string
+};
+
+export type ChangeTodolistFilterActionType = {
+    type: 'CHANGE-TODOLIST-FILTER'
+    id: string
+    filter: FilterValueType
+};
+
 
 // меня вызовут и дадут мне стейт (почти всегда объект)
 // и инструкцию (action, тоже объект)
