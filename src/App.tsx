cb3be1414bd19f96a796446a91d423e899950130
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-12-21 17:40:41 +0300

    feat: add useReducer (Thu, Dec 21, 2023, 05:39:16 PM)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import {TaskType, Todolist} from './Todolist';
 import {v1} from "uuid";
 import {AddItemForm} from "./components/AddItemForm/AddItemForm";
 import {Header} from "./components/Header/header";
-import Trash from "./assets/trash";
 import {PlusSquareOutline} from "./assets";
+import {
+    addTodolistAC,
+    changeTodolistFilterAC,
+    changeTodolistTitleAC,
+    removeTodolistAC,
+    todolistReducer
+} from "./state/todolists-reducer";
+import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, tasksReducer} from "./state/task-reducer";
 
 export type FilterPropsType = 'all' | 'active' | 'completed'
 
@@ -21,12 +28,12 @@ export const App = () => {
     let todolistID1 = v1()
     let todolistID2 = v1()
 
-    let [todolists, setTodolists] = useState<TodolistsType[]>([
+    let [todolists, dispatchToTodolists] = useReducer(todolistReducer, [
         {id: todolistID1, title: 'What to learn', filter: 'all'},
         {id: todolistID2, title: 'What to buy', filter: 'all'},
     ])
 
-    let [tasks, setTasks] = useState({
+    let [tasks, dispatchToTasks] = useReducer(tasksReducer, {
         [todolistID1]: [
             {id: v1(), title: 'HTML&CSS', isDone: true},
             {id: v1(), title: 'JS', isDone: true},
@@ -41,51 +48,40 @@ export const App = () => {
 
 //task
     const removeTask = (todolistId: string, taskId: string) => {
-        setTasks({...tasks, [todolistId]: tasks[todolistId].filter(el => el.id !== taskId)})
+        dispatchToTasks(removeTaskAC(todolistId, taskId))
     }
     const addTask = (todolistId: string, title: string) => {
-        setTasks({
-            ...tasks,
-            [todolistId]: [{id: v1(), title: title, isDone: false}, ...tasks[todolistId]]
-        })
+        dispatchToTasks(addTaskAC(todolistId, title))
     }
     const changeTaskStatus = (todolistId: string, taskId: string, isDone: boolean) => {
-        setTasks({
-            ...tasks,
-            [todolistId]: tasks[todolistId].map(task => task.id === taskId ? {...task, isDone} : task)
-        })
+        dispatchToTasks(changeTaskStatusAC(todolistId, taskId, isDone))
     }
-    const changeTaskTitle = (todolistId: string, taskId: string, title:string) => {
-        setTasks({
-            ...tasks,
-            [todolistId]: tasks[todolistId].map(task => task.id === taskId ? {...task, title} : task)
-        })
+    const changeTaskTitle = (todolistId: string, taskId: string, title: string) => {
+        dispatchToTasks(changeTaskTitleAC(todolistId, taskId, title))
     }
 
 //todo
     const changeFilter = (todolistId: string, filter: FilterPropsType) => {
-        setTodolists(todolists.map(el => el.id === todolistId ? {...el, filter} : el))
+        dispatchToTodolists(changeTodolistFilterAC(todolistId, filter))
     }
     const addTodolist = (title: string) => {
-        let newTodolistId = v1()
-        setTodolists([{id: newTodolistId, title, filter: 'all'}, ...todolists])
-        setTasks({
-            ...tasks,
-            [newTodolistId]: []
-        })
+        dispatchToTasks(addTodolistAC(title))
+        dispatchToTodolists(addTodolistAC(title))
     }
     const removeTodolist = (id: string) => {
-        setTodolists(todolists.filter(el => el.id !== id))
+        dispatchToTasks(removeTodolistAC(id))
+        dispatchToTodolists(removeTodolistAC(id))
     }
     const changeTodolistTitle = (todolistId: string, title: string) => {
-        setTodolists(todolists.map(el => el.id === todolistId ? {...el, title} : el))
+        dispatchToTodolists(changeTodolistTitleAC(todolistId, title))
     }
 
     return (
         <div className="App">
             <Header/>
             <div className="home__container">
-                <AddItemForm addItem={addTodolist} label={'New Todolist'} trigger={<PlusSquareOutline className={'icon'}/>}/>
+                <AddItemForm addItem={addTodolist} label={'New Todolist'}
+                             trigger={<PlusSquareOutline className={'icon'}/>}/>
                 {todolists.map(todolist => {
 
                     let allTodolistsTasks = tasks[todolist.id];
