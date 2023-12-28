4a9aeab2b943d3da6b761f7e1d0ac23f93af29a0
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-12-28 17:59:57 +0300

    feat: routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,32 +1,40 @@
-import { useEffect } from "react";
-import { TaskType } from "@/api/todolist-api";
-import { RequestStatusType } from "@/app/app-reducer";
-import { useAppDispatch, useAppSelector } from "@/app/store";
-import { ErrorSnackbar } from "@/components/ErrorSnackbar/ErrorSnackbar";
-import { Header } from "@/components/Header";
-import { Linear } from "@/components/Preloader";
-import { fetchTodolist } from "@/features/TodolistList/Todolist/todolists-reducer";
-import { TodolistList } from "@/features/TodolistList/TodolistList";
+import { useEffect } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+import { TaskType } from '@/api/todolist-api'
+import { initializedTC } from '@/app/app-reducer'
+import { useAppDispatch, useAppSelector } from '@/app/store'
+import { ErrorSnackbar } from '@/components/ErrorSnackbar/ErrorSnackbar'
+import { Header } from '@/components/Header'
+import { Linear } from '@/components/Preloader'
+import { Login } from '@/features/Login/login'
+import { TodolistList } from '@/features/TodolistList/TodolistList'
 
 export type TasksStateType = {
   [key: string]: TaskType[]
 }
 export const App = () => {
-  const status = useAppSelector<RequestStatusType>(state => state.app.status)
+  const isInitialized = useAppSelector<boolean>(state => state.app.isInitialized)
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    dispatch(fetchTodolist)
-  }, [dispatch])
+    dispatch(initializedTC())
+  }, [])
+
+  if (!isInitialized) {
+    return <Linear />
+  }
 
   return (
     <div className={'App'}>
       <ErrorSnackbar />
       <Header />
-      {status === 'loading' && <Linear className={'preloader'} />}
-      <TodolistList />
+      <Routes>
+        <Route element={<TodolistList />} path={'/'} />
+        <Route element={<Login />} path={'/login'} />
+        <Route element={<h1>404: PageNote Found</h1>} path={'/404'} />
+        <Route element={<Navigate to={'404'} />} path={'*'} />
+      </Routes>
     </div>
   )
 }
-
-
