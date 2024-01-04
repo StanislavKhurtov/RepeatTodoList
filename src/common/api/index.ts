90aae3a020ae8994eb18431c86b3bb9e900d35d1
Stanislav Khurtov <skhurtov.work@yandex.ru>
2024-01-04 21:24:55 +0300

    refactor: change structure project

---
+++ src/common/api/index.ts
@@ -0,0 +1,8 @@
+export * from './todolist-api'
+export * from '@/common/api/auth-api'
+export * from '@/common/api/common.api'
+export * from '@/common/api/task-api'
+export { RESULT_CODE } from '@/common/enums/common.enums'
+export { TaskPriorities } from '@/common/enums/common.enums'
+export { TaskStatuses } from '@/common/enums/common.enums'
+export { ResponseType } from '@/common/types/common.types'
